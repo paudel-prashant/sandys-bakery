381a570c796d2498e69deaed224636361555144c
Prashant Paudel <ppaudel.ca@gmailcom>
2022-11-11 15:01:17 -0500

    Initial commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SandysBakery.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit https://go.microsoft.com/fwlink/?LinkID=303951
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/WebFormsJs").Include(
+                            "~/Scripts/WebForms/WebForms.js",
+                            "~/Scripts/WebForms/WebUIValidation.js",
+                            "~/Scripts/WebForms/MenuStandards.js",
+                            "~/Scripts/WebForms/Focus.js",
+                            "~/Scripts/WebForms/GridView.js",
+                            "~/Scripts/WebForms/DetailsView.js",
+                            "~/Scripts/WebForms/TreeView.js",
+                            "~/Scripts/WebForms/WebParts.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Global").Include(
+                            "~/vendor/jquery/jquery.min.js",
+                            "~/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                            "~/vendor/jquery-easing/jquery.easing.min.js",
+                            "~/Scripts/js/sb-admin-2.js"));
+
+            // Order is very important for these files to work, they have explicit dependencies
+            bundles.Add(new ScriptBundle("~/bundles/MsAjaxJs").Include(
+                 "~/Scripts/WebForms/MsAjax/MicrosoftAjax.js",
+                 "~/Scripts/WebForms/MsAjax/MicrosoftAjaxApplicationServices.js",
+                 "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
+                 "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
+
+            // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                            "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/Global").Include(
+                            "~/vendor/fontawesome-free/css/all.min.css",
+                            "~/Content/css/sb-admin-2.min.css"));
+
+        }
+    }
+}
---
+++ Scripts/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar();
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
+
+        $('#page-footer').toggleClass('active');
+
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
